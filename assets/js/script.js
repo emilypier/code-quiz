24ee01d1060508cb037b473a7e232172cbf4dadc
Your Name <you@example.com>
2021-12-22 19:01:54 -0500

    made scores display on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-const startButton = document.querySelector("#start-btn");
-const questionContainerElement = document.querySelector("#question-container");
-const questionElement = document.querySelector("#question");
-const answerButtonsElement = document.querySelector("#answer-buttons");
-const scoresContainerElement = document.querySelector("#scores-container");
+var startButton = document.querySelector("#start-btn");
+var questionContainerElement = document.querySelector("#question-container");
+var questionElement = document.querySelector("#question");
+var answerButtonsElement = document.querySelector("#answer-buttons");
+var scoresContainerElement = document.querySelector("#scores-container");
 var timerElement = document.getElementById("timer");
-let timeLeft = 75;
-let shuffledQuestions;
+var timeLeft = 75;
+var timer;
 var currentQuestionIndex = 0;
 var highScoresButton = document.querySelector("#highscores-button");
 var doneButton = document.querySelector("#done-button");
 
 //quiz questions
-const questions = [
+var questions = [
   {
     question: "Commonly used data types DO not include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
@@ -21,40 +21,45 @@ const questions = [
   {
     question: "The condition in an if / else statement is enclosed with _____.",
     choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    answer: "parenthesis"
+    answer: "parenthesis",
   },
 
   {
     question: "Arrays in JavaScript can be used to store _____.",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: "all of the above"
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
   },
 
   {
-    question: "String values must be enclosed within _____. when being assigned to variables.",
+    question:
+      "String values must be enclosed within _____. when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-    answer: "quotes"
+    answer: "quotes",
   },
 
   {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal/bash", "for loops", "console.log" ],
-    answer: "console.log"
-  }
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
 ];
 
 function startGame() {
   scoresContainerElement.classList.add("hide");
   startButton.classList.add("hide"); //hide start button
   timer();
-  //shuffledQuestions = questions.sort(() => Math.random() - .5) //shuffles all questions
-  //starting on first question in shuffled questions array.
   questionContainerElement.classList.remove("hide"); //unhides question container
   showQuestion();
 }
 
 function timer() {
-  const timer = setInterval(function () {
+  timer = setInterval(function () {
     timeLeft = timeLeft - 0.0; //changed to .0 because 1 made it count down by 2 secs
     if (timeLeft > 0) {
       timerElement.textContent = "Time: " + timeLeft;
@@ -84,7 +89,6 @@ function showQuestion() {
   });
 }
 
-
 function selectAnswer() {
   const selectedButton = this.value;
 
@@ -103,20 +107,6 @@ function selectAnswer() {
   } else {
     showQuestion();
   }
-};
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element);
-  if (correct) {
-  } else {
-    questionElement.classList.add("wrong");
-    //deduct 10 seconds from score
-  }
-}
-
-function clearStatusClass(element) {
-  questionElement.classList.remove("correct");
-  questionElement.classList.remove("wrong");
 }
 
 function endGame() {
@@ -124,36 +114,59 @@ function endGame() {
   scoresContainerElement.classList.remove("hide");
   timerElement.classList.add("hide");
   clearInterval(timer); // stops timer
-  //.log(timeLeft);
+  loadScores();
 }
 
 //setItem(key, value). save initials to localStorage
 function saveInitials() {
   var initialsInput = document.querySelector("#initials").value;
 
-  // var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-  // var newScore = {
-  //   initials: initialsInput,
-  //   score: timeLeft
-  // };
-  console.log(timeLeft)
+  var newScore = {
+    initials: initialsInput,
+    score: timeLeft,
+  };
+  console.log(timeLeft);
 
-  // highscores.push(newScore)
+  highscores.push(newScore);
 
-  // localStorage.setItem("highscores", highscores);
+  window.localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+//dynamically make score table & load initials and scores
+function loadScores() {
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  //sort highest to lowest
+  highscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  var scoresTable = document.getElementById("scoresTable");
+  var tableRow = document.createElement("tr");
+  var initialsColumn = document.createElement("td");
+  var scoreColumn = document.createElement("td");
+
+  initialsColumn.textContent = "Initials";
+  scoreColumn.textContent = "Score";
+
+  tableRow.append(initialsColumn, scoreColumn);
+  scoresTable.append(tableRow);
+
+  highscores.forEach(function (score) {
+    var playerRow = document.createElement("tr");
+    var userInitialsColumn = document.createElement("td");
+    var userScoreColumn = document.createElement("td");
+
+    userInitialsColumn.textContent = score.initials;
+    userScoreColumn.textContent = score.score;
+
+    playerRow.append(userInitialsColumn, userScoreColumn);
+    scoresTable.append(playerRow);
+  });
+  // window.location.reload()
 }
 
 startButton.addEventListener("click", startGame);
 doneButton.addEventListener("click", saveInitials);
-
-//getItem(key)
-
-//removeItem(key)
-
-//key(index)
-
-// function endGame() {
-//   questionContainerElement.classList.add('hide');
-//   scoresContainerElement.classList.remove('hide');
-// };
